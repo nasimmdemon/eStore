8815847d4f3465325f9391d6c20bb8ef654e9bc1
Emon Adi <me@emonadi.com>
2024-01-15 14:37:48 +0600

    Project setup and native splash screen is done

---
+++ lib/app.dart
@@ -0,0 +1,28 @@
+import 'package:estore/utils/constants/text_strings.dart';
+import 'package:estore/utils/theme/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: TTexts.appName,
+      themeMode: ThemeMode.system,
+      theme: TAppTheme.lightTheme,
+      darkTheme: TAppTheme.darkTheme,
+      debugShowCheckedModeBanner: false,
+      // initialBinding: GeneralBindings(),
+      home: const Scaffold(
+        body: Center(
+          child: Text(
+            'Awesome! 🎊 Project Structure is set up and running. \n Happy Coding Emon 🎊',
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
